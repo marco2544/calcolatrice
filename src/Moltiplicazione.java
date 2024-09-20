6892eac96b475180a39d5c1d984f98c20eb5941d
marco <marcoghidini6@gmail.com>
2024-09-20 16:49:18 +0200

    https://github.com/marco2544/calcolatrice.git

--- src/Moltiplicazione.java
+++ src/Moltiplicazione.java
@@ -1,7 +1,4 @@
 public class Moltiplicazione implements  Operatore{
-    double a;
-    double b;
-
 
     public Double calcolo(double a, double b) {
         return a*b;
--- src/Sottrazione.java
+++ src/Sottrazione.java
@@ -1,8 +1,4 @@
 public class Sottrazione implements  Operatore{
-    double a;
-    double b;
-
-
 
     public Double calcolo(double a, double b) {
         return a-b;
